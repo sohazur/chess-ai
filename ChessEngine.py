587b0e2ac47e918120aa82a6f9d1b57f9e2a35c0
Sohazur <mdsohazurislam@gmail.com>
2023-02-19 02:00:35 +0400

    Updated ChessEngine to add Move class

--- ChessEngine.py
+++ ChessEngine.py
@@ -20,3 +20,27 @@ class GameState():
         ]
         self.whiteToMove = True
         self.moveLog = []
+
+class Move():
+    # maps keys to values
+    # key : value
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, 
+                   "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, 
+                   "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+    
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startColumn = startSq[1]
+        self.endRow = endSq[0]
+        self.endColumn = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startColumn]
+        self.pieceCaptured = board[self.endRow][self.endColumn]
+    
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startColumn) + self.getRankFile(self.endRow, self.endColumn)
+    
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
